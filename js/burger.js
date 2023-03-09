0548eae8e50988a26e5aa75e093748dc806917fa
Insa <ilya2002i52@mail.ru>
2023-03-09 22:49:53 +0500

    navigation

--- js/burger.js
+++ js/burger.js
@@ -3,9 +3,16 @@ function nav() {
 	const burger = document.querySelector(".burger");
 	const content = document.querySelector(".burger__content");
 	const links = content.querySelectorAll(".burger__links");
+	const body = document.querySelector('body');
 
 	burger.addEventListener("click", () => {
 		content.classList.toggle("show");
+		if(content.classList.contains('show')){
+			body.style.overflow ='hidden';
+		}else{
+			body.style.overflowY ='scroll';
+		}
+		
 	});
 
 	links.forEach((link) => {
